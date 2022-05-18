42935f634f564988dcd89b9736bb940e69a96f5f
SupremeDegen <sunny@flowstation.io>
2022-05-18 16:32:31 +0800

    Added tests for deploy

--- test/StarToken.spec.ts
+++ test/StarToken.spec.ts
@@ -11,11 +11,24 @@ describe("StarToken Tests", () =>{
 
     const oneStarTokenInWei = ethers.utils.parseEther("1");
     const MINTER_ROLE = ethers.utils.solidityKeccak256(['string'],['MINTER_ROLE']);
+    const DEFAULT_ADMIN_ROLE = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
 
     beforeEach(async () => {
         starTokenContract = await getStarToken({ contractName: "StarToken", deployParams: [] });
         [deployer, acc1] = await ethers.getSigners();
     });
+
+
+    describe("deploy", () => {
+        it('deployer has the DEFAULT_ADMIN_ROLE role', async function () {
+            expect(await starTokenContract.hasRole(DEFAULT_ADMIN_ROLE, deployer.address.toLowerCase())).to.be.true;
+        });
+
+        it('non-deployer to not have DEFAULT_ADMIN_ROLE role', async function () {
+            expect(await starTokenContract.hasRole(DEFAULT_ADMIN_ROLE, acc1.address.toLowerCase())).to.be.false;
+        });
+    })
 
     describe("mint", () => {
         it("Should revert if the caller does not have minter role", async () => {
@@ -26,11 +39,9 @@ describe("StarToken Tests", () =>{
 
         it("Should work when called by the owner", async () => {
         await starTokenContract.mint(deployer.address, oneStarTokenInWei);
-    
+
         const balance = await starTokenContract.balanceOf(deployer.address);
         expect(balance).to.equal(oneStarTokenInWei);
         });    
     })
-
-
 });
